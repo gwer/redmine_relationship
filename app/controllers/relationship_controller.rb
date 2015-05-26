4c1fdca2ebac5494206db05e009dc14729072824
gwer <gwer@webholt.ru>
2015-05-26 15:22:10 +0300

    И не закончив мастерить устройство бесовское, Чтоб наработки не смогли в пучине сгинуть, Ремесленник, глядя на них с тоскою, Спешит их поскорее в гите закоммитить.

--- app/controllers/relationship_controller.rb
+++ app/controllers/relationship_controller.rb
@@ -6,17 +6,50 @@ class RelationshipController < ApplicationController
   end
 
   def projects
-  	@projects = Project.all
-  	render json: @projects.reduce({}) {|a,v| a.merge({(v.parent_id!=nil) ? v.parent_id : 'null' => [v]}){|_, old, new| old+new}}
+  	@projects = Project.all(conditions: {status: 1})
+    result = {}
+  	#render json: @projects.reduce({}) {|a,v| a.merge({(v.parent_id!=nil) ? v.parent_id : 'null' => [v]}){|_, old, new| old+new}}
+    @projects.each do | project |
+      key = (project.parent_id!=nil) ? project.parent_id : 'null'
+      if result.has_key?(key)
+        result[key][:projects] << {
+          project: project,
+          has_content: project_has_content(project.id)
+        }
+      else
+        result[key] = {
+          projects: [{
+            project: project,
+            has_content: project_has_content(project.id)
+          }],
+          issues_loaded: false
+        }
+      end
+    end
+    render json: result
   end
 
   def projects_children
   	@issues = Issue.all(conditions: {project_id: params[:id]})
-  	render json: @issues
+  	result = @issues.map do | issue | {
+        issue: issue,
+        has_content: issue_has_content(issue.id)
+      }
+    end
+    render json: result
   end
 
   def issues_children
   	@issues = Issue.all(conditions: {parent_id: params[:id]})
   	render json: @issues
+  end
+
+private
+  def project_has_content(id)
+    has_content = Project.all(conditions: {parent_id: id}).count > 0
+    has_content ||= Issue.all(conditions: {project_id: id}).count > 0
+  end
+  def issue_has_content(id)
+    Issue.all(conditions: {parent_id: id}).count > 0
   end
 end
--- assets/javascripts/relationship.js
+++ assets/javascripts/relationship.js
@@ -1,16 +1,55 @@
 jQuery.noConflict();
 
 jQuery(function($){
-	$.get('/relationship/projects', function(data){
-		function draw_tree(data, parent) {
-			var tree = $('<ul>')
-			data[parent] && data[parent].each(function(el){
-				var branch = $('<li data-id="' + el.id + '">' + el.name + '</li>')
-				branch.append(draw_tree(data, el.id))
-				tree.append(branch)
-			})
-			return tree
+
+	var projects = {},
+		issues = {}
+
+	function draw_branch(type, id, base) {
+		var branch = $('<ul>')
+		var _switcher = $('<div class="switcher">')
+		load_content(type, id)
+		base.children('ul').remove()
+		if (id !== null) branch.css('display', 'none')
+		projects[id] && projects[id].projects.each(function(el){
+			var project = el.project
+			var switcher = _switcher.clone()
+			var leaf = $('<li>' + project.name + '</li>')
+				.data('id', project.id)
+				.data('type', 'project')
+				.prepend(switcher)
+			if (!el.has_content) {
+				switcher.addClass('hidden')
+			} 
+			switcher.click(switch_handler)
+			branch.append(leaf)
+		})
+		base.append(branch)
+		base.data('ready', true)
+	}
+
+	function switch_handler() {
+		var base = $(this).closest('li')
+		if (!base.data('ready')) {
+			draw_branch(base.data('type'), base.data('id'), base)
 		}
-		$('.column .tree').append(draw_tree(data, null))
+		$(this).toggleClass('open')
+		base.children('ul').slideToggle()
+	}
+
+	function load_content(type, id) {
+		var url = (type == 'project') ? 
+					'relationship/projects/' + id + '/children' :
+					'relationship/issues/' + id + '/children'
+		var objects = (type == 'project') ? projects : issues
+		$.get(url, function(data){
+			objects[id].issues = data
+			console.log(data)
+		})
+	}
+
+	$.get('/relationship/projects', function(data){
+		projects = data
+		draw_branch('project', null, $('.column .tree'))
 	})
 })
